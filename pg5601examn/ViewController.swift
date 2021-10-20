8706b16f13e5201a231251d2b73dca0bf48c061f
olavhl <56083131+olavhl@users.noreply.github.com>
2021-10-20 11:50:06 +0200

    Initial Commit

---
+++ pg5601examn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pg5601examn
+//
+//  Created by Olav Hartwedt Larsen on 20/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pg5601examnUITests/pg5601examnUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pg5601examnUITestsLaunchTests.swift
+//  pg5601examnUITests
+//
+//  Created by Olav Hartwedt Larsen on 20/10/2021.
+//
+
+import XCTest
+
+class pg5601examnUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
