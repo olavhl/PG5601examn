58c9a9dda85e197758be3937a2b5ef70c0fd3230
olavhl <56083131+olavhl@users.noreply.github.com>
2021-10-21 12:23:54 +0200

    Adding tableview to the project, not ready tho

--- pg5601examn/Controller/ViewController.swift
+++ pg5601examn/Controller/ViewController.swift
@@ -7,16 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     let userManager = UserManager()
-
+    
+    let myData = ["1", "2", "3", "4", "5"]
+    
+    @IBOutlet weak var userTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         userManager.fetchAllUsers()
+        
+        userTableView.delegate = self
+        userTableView.dataSource = self
     }
-
-
+    
+    // Returning 100 rows in a section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = myData[indexPath.row]
+        return cell
+    }
+    
 }
 
