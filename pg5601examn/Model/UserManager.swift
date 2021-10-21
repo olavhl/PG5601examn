825514e4898a51d0a51531633b98f3cc52a2f337
olavhl <56083131+olavhl@users.noreply.github.com>
2021-10-21 11:41:35 +0200

    Implemented a UserModel to be able to have my own names for the parameters

--- pg5601examn/Model/UserManager.swift
+++ pg5601examn/Model/UserManager.swift
@@ -10,19 +10,55 @@ import Foundation
 struct UserManager {
     let baseUrl = "https://randomuser.me/api/?results=100&nat=no"
     
-    func getUsers() {
-        if let url = URL(string: baseUrl) {
+    func fetchAllUsers() {
+        performRequest(with: baseUrl)
+    }
+    
+    func performRequest(with url: String) {
+        if let url = URL(string: url) {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url){ (data, response, error) in
                 if error != nil {
                     print(error!)
                     return
                 }
-                let dataAsString = String(data: data!, encoding: .utf8)
-                print(dataAsString)
+                print(fetchJSON(data!))
             }
+            
             task.resume()
         }
-        
     }
+    
+    func fetchJSON(_ data: Data) -> [UserModel]? {
+        do {
+            let userData = try JSONDecoder().decode(Users.self, from: data)
+            let userResults = userData.results
+            
+            var users: [UserModel] = []
+            
+            for user in userResults {
+                let firstName = user.name.first
+                let lastName = user.name.last
+                let picture = user.picture.large
+                let email = user.email
+                let birthDate = user.dob.date
+                let phoneNumber = user.cell
+                let city = user.location.city
+                let coordinateLatitude = user.location.coordinates.latitude
+                let coordinateLongitude = user.location.coordinates.longitude
+                
+                let implementedUser = UserModel(firstName: firstName, lastName: lastName, picture: picture, email: email, birthDate: birthDate, phoneNumber: phoneNumber, city: city, coordinateLatitude: coordinateLatitude, coordinateLongitude: coordinateLongitude)
+                
+                users.append(implementedUser)
+            }
+            
+            return users
+        } catch {
+            print(error)
+            return nil
+        }
+        
+
+    }
+
 }
---
+++ pg5601examn/Model/UserModel.swift
@@ -0,0 +1,21 @@
+//
+//  UserModel.swift
+//  pg5601examn
+//
+//  Created by Olav Hartwedt Larsen on 21/10/2021.
+//
+
+import Foundation
+
+// Implementing a new Model to be able to have my own namings for the objects
+struct UserModel {
+    let firstName: String
+    let lastName: String
+    let picture: String
+    let email: String
+    let birthDate: String
+    let phoneNumber: String
+    let city: String
+    let coordinateLatitude: String
+    let coordinateLongitude: String
+}
